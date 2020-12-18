5a1d60035d9bcec729e225b3b0cd9749cdf1706a
Ilyas Yambay <ilyas.yambay@gmail.com>
2020-12-18 09:42:33 +0100

    Remove assertions for an interview.

--- src/test/java/dev/yambay/collections/MapTest.java
+++ src/test/java/dev/yambay/collections/MapTest.java
@@ -15,20 +15,20 @@ class MapTest {
 	@Test
 	void test() {
 		Map<K, V> map = new HashMap<>();
+		
 		K k1 = new K(1);
 		K k2 = new K(2);
+		
 		V v11 = new V("11");
 		V v12 = new V("12");
 		V v21 = new V("21");
 		V v22 = new V("22");
+
 		map.put(k1, v11);
 		map.put(k2, v21);
-
-		assertEquals(2, map.size());
 
 		map.put(k1, v12);
 		map.put(new K(2), v22);
 
-		assertEquals(3, map.size());
 	}
 }
